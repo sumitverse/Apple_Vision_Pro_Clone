7c554ce856059878593c7de7e725f277ac2ed875
Sumit Kumar <132631813+sumitverse@users.noreply.github.com>
2025-07-11 11:09:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 function loco() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  },
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
 }
@@ -358,7 +347,6 @@ ease: `none`,
 scrollTrigger: {
   scrub: 0.15,
   trigger: `#page7>canvas`,
-  //   set start end according to preference
   start: `top top`,
   end: `600% top`,
   scroller: `#main`,
@@ -398,7 +386,6 @@ trigger: "#page7>canvas",
 pin: true,
 // markers:true,
 scroller: `#main`,
-//   set start end according to preference
 start: `top top`,
 end: `600% top`,
 });
@@ -471,7 +458,6 @@ ease: `none`,
 scrollTrigger: {
   scrub: 0.15,
   trigger: `#page18`,
-  //   set start end according to preference
   start: `top top`,
   end: `80% top`,
   scroller: `#main`,
@@ -511,7 +497,6 @@ trigger: "#page18",
 pin: true,
 // markers:true,
 scroller: `#main`,
-//   set start end according to preference
 start: `top top`,
 end: `80% top`,
 });
